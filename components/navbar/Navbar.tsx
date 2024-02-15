ee0ed5ddbbc232f545444e83c779143f367faaaa
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-02-15 10:49:35 +0100

    installed octicons for navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,17 +1,29 @@
+import Link from 'next/link';
+
+import { HomeIcon } from '@primer/octicons-react';
+
+const navItems = [
+  { path: './about', text: 'About' },
+  { path: './pricing', text: 'Pricing' },
+  { path: './contact', text: 'Contact' },
+];
+
 export function Navbar() {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
-      <span>Home</span>
+      <Link href={'./'} className='flex items-center'>
+        <span>
+          <HomeIcon className='mr-2' size={18} />
+          Home
+        </span>
+      </Link>
+
       <div className='flex flex-end flex-1' />
-      <a className='mr-2' href='/about'>
-        About
-      </a>
-      <a className='mr-2' href='/pricing'>
-        Pricing
-      </a>
-      <a className='mr-2' href='/contact'>
-        Contact
-      </a>
+      {navItems.map((item) => (
+        <Link key={item.text} href={item.path} className='mr-2'>
+          {item.text}
+        </Link>
+      ))}
     </nav>
   );
 }
