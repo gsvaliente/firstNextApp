b55c8711be29d4aec4ae48afe8eaeaa61bcae518
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-02-15 09:55:05 +0100

    created a navbar with simple <a> movement

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,10 +1,15 @@
+import { Navbar } from '@/components';
+
 export default function AboutLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className='flex flex-col items-center p-24'>
-      <h1 className='text-lg'>Hello, world</h1>
-      {children}
-    </main>
+    <>
+      <Navbar />
+      <main className='flex flex-col items-center p-24'>
+        <h1 className='text-lg'>Hello, world</h1>
+        {children}
+      </main>
+    </>
   );
 }
