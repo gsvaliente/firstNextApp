dc45458e01ad1a1e0a520b17098681ccdeef501c
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-02-21 10:08:09 +0100

    added a button to get to about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <span className='text-5xl'>Hello World</span>
+      <Link href={'/about'}>About</Link>
     </main>
   );
 }
