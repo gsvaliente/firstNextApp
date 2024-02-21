d639aafc7503a02bc126f378b7a544bd9394433d
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-02-21 11:05:21 +0100

    added my name in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
       <span className='text-5xl'>Hello World</span>
+      <span className='text-2xl'>I am Gabriel Valiente</span>
       <Link className='mt-5 hover:text-blue-500' href={'/about'}>
         About
       </Link>
